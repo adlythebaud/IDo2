3bc1b07c8167d27bbf86af0403fe8a4c7d019631
Prestonh91 <preston.higgins91@gmail.com>
2017-11-17 01:42:37 -0700

    Login activity working

---
+++ app/src/main/java/com/mycabbages/teamavatar/ido2/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.mycabbages.teamavatar.ido2;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    public final static String USERLOG = "User_log";
+    private FirebaseAuth myAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        myAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = myAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void signUpNewUser(View view) {
+        EditText _email = (EditText)findViewById(R.id.emailInput);
+        EditText _password = (EditText)findViewById(R.id.passwordInput);
+        String email = _email.getText().toString();
+        String password = _password.getText().toString();
+        Log.d(USERLOG, email);
+        Log.d(USERLOG, password);
+
+
+        Log.d(USERLOG, "About to go into Firebase Create User");
+        myAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(USERLOG, "Inside the oncomplete: " + task.isSuccessful());
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(USERLOG, "createUserWithEmail:success");
+                            FirebaseUser user = myAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(USERLOG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void signInExistingUser(View view) {
+        EditText _email = (EditText)findViewById(R.id.emailInput);
+        EditText _password = (EditText)findViewById(R.id.passwordInput);
+        String email = _email.getText().toString();
+        String password = _password.getText().toString();
+        Log.d(USERLOG, email);
+        Log.d(USERLOG, password);
+
+        myAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(USERLOG, "signInWithEmail:success");
+                            FirebaseUser user = myAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(USERLOG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
+}
