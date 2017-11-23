39c6ba88425de2b7d2c0d280d1e6050eda3f8079
msheridan <mspencersheridan@gmail.com>
2017-11-23 10:55:20 -0700

    Fixing TextMessage class

--- app/src/main/java/com/mycabbages/teamavatar/ido2/TextMessage.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido2/TextMessage.java
@@ -3,34 +3,32 @@ package com.mycabbages.teamavatar.ido2;
 import java.util.Date;
 
 /**
- * Created by Preston on 11/17/2017.
+ * Created by Madison on 11/23/2017.
  */
 
-public class TextMessage implements Message {
+public class TextMessage {
     private String _messageText;
     private String _messageUser;
-    private long _timeTextReceived;
-    private long _timeTextSent;
+    private long _messageTime;
 
     TextMessage() {
         _messageText = "";
         _messageUser = "";
-        _timeTextReceived = (long) 0.0;
-        _timeTextSent = (long) 0.0;
+        _messageTime = (long) 0.0;
     }
 
     TextMessage(String messageText, String messageUser) {
-        _messageText = messageText;
-        _messageUser = messageUser;
-        _timeTextReceived = (long) 0.0;
-        _timeTextSent = (long) 0.0;
+        this._messageText = messageText;
+        this._messageUser = messageUser;
+        this._messageTime = new Date().getTime();
     }
 
-    public void setDateReceived(Date dateReceived) { _timeTextReceived = dateReceived.getTime(); }
-    public Date getDateReceived() { return new Date(_timeTextReceived); }
+    public String getMessageText() { return _messageText; }
+    public void setMessageText(String messageText) { this._messageText = messageText; }
 
-    public Date getDateSent() { return new Date(_timeTextSent); }
-    public void setDateSent(Date dateSent) { _timeTextSent = dateSent.getTime(); }
+    public String getMessageUser() { return _messageUser; }
+    public void setMessageUser(String messageUser) { this._messageUser = messageUser; }
 
-    public void sendMessage(String message) {}
+    public long getMessageTime() { return _messageTime; }
+    public void setMessageTime(long messageTime) { this._messageTime = messageTime; }
 }
