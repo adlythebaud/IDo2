7bef049c6e40d4982729cab4d0fd8267950dc677
adlythebaud <adlythebaud@gmail.com>
2017-11-28 23:20:06 -0700

    Current user and database references in MainActivity

--- app/src/main/java/com/mycabbages/teamavatar/ido2/fragment/GoalsFragment.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido2/fragment/GoalsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.mycabbages.teamavatar.ido2.R;
 
