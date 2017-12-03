aa8af6316cf906eba312d12ef2105f6161584142
Prestonh91 <preston.higgins91@gmail.com>
2017-12-02 17:20:00 -0700

    some goal fixes, and add goal fragment

--- app/src/main/java/com/mycabbages/teamavatar/ido2/Goal.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido2/Goal.java
@@ -2,29 +2,50 @@ package com.mycabbages.teamavatar.ido2;
 
 
 import java.util.Date;
+import java.util.Calendar;
 
 /**
  * Created by Conrad on 11/3/2017.
  */
 
 public class Goal {
-    private Date date;
-    private String title;
-    private Boolean completed;
+    private Date mDate;
+    private String mTitle;
+    private boolean mCompleted;
+    private Calendar mDateAndTimeToComplete;
+
+    public Goal() {
+        mDate = null;
+        mTitle = "";
+        mCompleted = false;
+        mDateAndTimeToComplete = null;
+    }
+
+    public Goal(Date date, String title, boolean completed, Calendar DateAndTimeToComplete) {
+        mDate = date;
+        mTitle = title;
+        mCompleted = completed;
+        mDateAndTimeToComplete = DateAndTimeToComplete;
+    }
 
     public Date getDate(){
-        return date;
+        return mDate;
     }
     public void setDate(Date date){
-        this.date = date;
+        this.mDate = date;
     }
 
-    public String getGoalTitle() { return title; }
+    public String getGoalTitle() { return mTitle; }
     public void setGoalTitle(String title){
-        this.title = title;
+        this.mTitle = title;
     }
 
-    public boolean getCompleted(){ return completed; }
-    public void setCompleted(Boolean completed) { this.completed = completed; }
+    public boolean getCompleted(){ return mCompleted; }
+    public void setCompleted(Boolean completed) { this.mCompleted = completed; }
+
+    public Calendar getDateAndTimeToComplete () { return mDateAndTimeToComplete; }
+    public void setDateAndTimeToComplete (Calendar dateAndTimeToComplete) {
+        this.mDateAndTimeToComplete = dateAndTimeToComplete;
+    }
 
 }
