9bd9b4646292a4064616e6b536badf781152939f
Prestonh91 <preston.higgins91@gmail.com>
2017-12-04 15:11:11 -0700

    fixed up add goals need to add actual adding of goal

--- app/src/main/java/com/mycabbages/teamavatar/ido2/AddGoalActivity.java
+++ app/src/main/java/com/mycabbages/teamavatar/ido2/AddGoalActivity.java
@@ -1,13 +1,28 @@
 package com.mycabbages.teamavatar.ido2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddGoalActivity extends AppCompatActivity {
+    static final String TAG = "Add_goal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goal);
+    }
+
+    public void validate(View view) {
+        EditText goalTitle = view.findViewById(R.id.GoalEditText);
+    }
+
+    public void cancel(View view) {
+        Intent intentToStartMainActivity = new Intent(AddGoalActivity.this,
+                MainActivity.class);
+        startActivity(intentToStartMainActivity);
     }
 }
